ea4f709036468f837fdf5b7a655ac0f7684143d9
José Augusto <augustoreal.santos@gmail.com>
2025-07-22 09:10:57 -0300

    Aula1

---
+++ CursoLua/Love2d/Aula1/main.lua
@@ -0,0 +1,13 @@
+_G.love = require("love")
+
+function love.load()
+    -- carregar todos os objetos do jogo e demais
+end
+
+function love.update()
+    -- É executada a cada FPS, é onde é trabalhado a mecânica do jogo em si
+end
+
+function love.draw()
+    -- É onde é "Desenhado" os objetos e demais do nosso jogo
+end
